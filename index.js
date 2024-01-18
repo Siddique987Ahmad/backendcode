480f1254e193bd8af4a2df191a477ea33d0fb867
Siddique Ahmad <siddiqueahmad956@gmail.com>
2024-01-18 18:06:15 +0500

    github data added

--- index.js
+++ index.js
@@ -2,12 +2,33 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
-
+const githubData={
+    "id": 12,
+    "title": "Brown Perfume",
+    "description": "Royal_Mirage Sport Brown Perfu...",
+    "price": 40,
+    "discountPercentage": 15.66,
+    "rating": 4,
+    "stock": 52,
+    "brand": "Royal_Mirage",
+    "category": "fragrances",
+    "thumbnail": "https://cdn.dummyjson.com/product-images/12/thumbnail.jpg",
+    "images": [
+      "https://cdn.dummyjson.com/product-images/12/1.jpg",
+      "https://cdn.dummyjson.com/product-images/12/2.jpg",
+      "https://cdn.dummyjson.com/product-images/12/3.png",
+      "https://cdn.dummyjson.com/product-images/12/4.jpg",
+      "https://cdn.dummyjson.com/product-images/12/thumbnail.jpg"
+    ]
+  }
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/twitter',(req,res)=>{
 res.send("siddahmad")
+})
+app.get('/github',(req,res)=>{
+res.json(githubData)
 })
 app.listen(process.env.port, () => {
   console.log(`Example app listening on port ${port}`)
